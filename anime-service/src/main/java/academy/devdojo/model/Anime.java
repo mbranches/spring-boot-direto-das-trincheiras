364b8088b57525d854ac0e48dc374b452b0e1944
Branches <marcus.branches@icloud.com>
2025-03-05 20:54:55 -0300

    feat: aula 23 - exercicio05 adicionando metodo delete ao anime controller

--- anime-service/src/main/java/academy/devdojo/model/Anime.java
+++ anime-service/src/main/java/academy/devdojo/model/Anime.java
@@ -2,18 +2,21 @@ package academy.devdojo.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Anime {
+    @EqualsAndHashCode.Include
     private Long id;
     private String name;
     public static List<Anime> animes = new ArrayList<>(List.of(new Anime(1L, "Naruto"), new Anime(2L, "Dbz")));
 
     public static List<Anime> listAllAnimes() {
-        return new ArrayList<>(List.of(new Anime(1L, "Naruto"), new Anime(2L, "Dbz")));
+        return animes;
     }
 }
---
+++ exercises/Exercise05.md
@@ -0,0 +1,4 @@
+
+# Exercise 5 - Delete
+
+Crie um endpoint para deletar um Anime da lista existente, usando o @DeleteMapping
