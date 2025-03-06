b3f875afa70586ef21e77501b4014c38f2d18d19
mbranches <marcusvbranches@gmail.com>
2025-03-06 11:24:31 -0300

    feat: video26 - adicionando producer repository

--- anime-service/src/main/java/academy/devdojo/model/Producer.java
+++ anime-service/src/main/java/academy/devdojo/model/Producer.java
@@ -17,19 +17,5 @@ public class Producer {
     private Long id;
     private String name;
     private LocalDateTime createdAt;
-    private static List<Producer> producers = new ArrayList<>();
-    static {
-        Producer mappa = Producer.builder().id(1L).name("Mappa").createdAt(LocalDateTime.now()).build();
-        Producer kyoto = Producer.builder().id(2L).name("Kyoto Animation").createdAt(LocalDateTime.now()).build();
-        Producer madHouse = Producer.builder().id(3L).name("MadHouse").createdAt(LocalDateTime.now()).build();
-        producers.addAll(List.of(mappa, kyoto, madHouse));
-    }
 
-    public static List<Producer> getProducers() {
-        return producers;
-    }
-
-    public static void addProducer(Producer producer) {
-        Producer.producers.add(producer);
-    }
 }
