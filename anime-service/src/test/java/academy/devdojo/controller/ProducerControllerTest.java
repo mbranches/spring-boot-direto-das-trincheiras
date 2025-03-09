a9da6412c6cfb59952f5630c07d937040e637bc5
mbranches <marcusvbranches@gmail.com>
2025-03-09 11:25:48 -0300

    feat: video 43 - resposta esperada externalizada a um arquivo json e datetime fixo adicionado

--- anime-service/src/test/java/academy/devdojo/controller/ProducerControllerTest.java
+++ anime-service/src/test/java/academy/devdojo/controller/ProducerControllerTest.java
@@ -13,30 +13,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 //Slices test -> é parecido com teste de integração, porém starta só os beans mais importantes
-@WebMvcTest(controllers = ProducerController.class) //se não definir, por pa    drão vai testar para todos os controllers
+@WebMvcTest(controllers = ProducerController.class) //se não definir, por padrão vai testar para todos os controllers
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @Import({ProducerMapperImpl.class, ProducerService.class, ProducerHardCodedRepository.class, ProducerData.class}) //não importa por padrões: service, component e repository
 class ProducerControllerTest {
     @MockBean //No contexto do spring utilizar esse
     private ProducerData producerData;
     @Autowired
-    private MockMvc mockMvc;private final List<Producer> PRODUCERS_LIST = new ArrayList<>();
+    private MockMvc mockMvc;
+    @Autowired
+    private ResourceLoader resourceLoader;
+    private final List<Producer> PRODUCERS_LIST = new ArrayList<>();
 
     @BeforeEach
     void init() {
-        Producer ufotable = Producer.builder().id(1L).name("Ufotable").createdAt(LocalDateTime.now()).build();
-        Producer witStudio = Producer.builder().id(2L).name("Wit Studio").createdAt(LocalDateTime.now()).build();
-        Producer studioGiblin = Producer.builder().id(3L).name("Studio Giblin").createdAt(LocalDateTime.now()).build();
+        String dateTime = "2025-03-09T11:05:34.1311715";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSS");
+        LocalDateTime localDateTime = LocalDateTime.parse(dateTime, formatter);
+
+        Producer ufotable = Producer.builder().id(1L).name("Ufotable").createdAt(localDateTime).build();
+        Producer witStudio = Producer.builder().id(2L).name("Wit Studio").createdAt(localDateTime).build();
+        Producer studioGiblin = Producer.builder().id(3L).name("Studio Giblin").createdAt(localDateTime).build();
         PRODUCERS_LIST.addAll(List.of(ufotable, witStudio, studioGiblin));
 
         BDDMockito.when(producerData.getPRODUCERS()).thenReturn(PRODUCERS_LIST);
@@ -46,9 +58,16 @@ class ProducerControllerTest {
     @DisplayName("findAll returns a list with all produces when name is null")
     @Order(1)
     void findAll_ReturnSAllProducers_WhenArgumentIsNull() throws Exception {
+        String responseExpected = readResourceFile("producer/get-producer-null-name-200.json");
+
         mockMvc.perform(MockMvcRequestBuilders.get("/v1/producers"))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].id").value(1L));
+                .andExpect(MockMvcResultMatchers.content().json(responseExpected));
+    }
+
+    private String readResourceFile(String fileName) throws IOException {
+        File file = resourceLoader.getResource("classpath:%s".formatted(fileName)).getFile();
+        return new String(Files.readAllBytes(file.toPath()));  
     }
 }
