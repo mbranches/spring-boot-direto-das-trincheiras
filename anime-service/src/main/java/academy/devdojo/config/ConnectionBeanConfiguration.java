0c8947fb569df12800f29210ec27bf2eecb1820e
mbranches <marcusvbranches@gmail.com>
2025-03-19 16:33:24 -0300

    feat: video54 - usando ConfigurationProperties

--- anime-service/src/main/java/academy/devdojo/config/ConnectionBeanConfiguration.java
+++ anime-service/src/main/java/academy/devdojo/config/ConnectionBeanConfiguration.java
@@ -1,28 +1,25 @@
 package academy.devdojo.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-public class ConnectionConfiguration {
-    @Value("${database.url}")
-    private String url;
-    @Value("${database.username}")
-    private String username;
-    @Value("${database.password}")
-    private String password;
-
+@RequiredArgsConstructor
+public class ConnectionBeanConfiguration {
+    private final ConnectionConfigurationProperties configurationProperties;
 //    @Profile("mysql")
     @Bean
     public Connection connectionMySql() {
-        return new Connection(url, username, password);
+        return new Connection(configurationProperties.url(), configurationProperties.username(), configurationProperties.password());
     }
 
     @Bean
     @Profile("mongo")
     public Connection connectionMongo() {
-        return new Connection(url, username, password);
+//        return new Connection(url, username, password);
+        return null;
     }
 }
