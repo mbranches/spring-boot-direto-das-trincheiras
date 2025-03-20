4177f87fcd38e50b3a9730e8c0828a425cf49a26
mbranches <marcusvbranches@gmail.com>
2025-03-20 11:13:10 -0300

    feat: video56 - Exercicio 12: teste unitario adicionado à UserController

--- user-service/src/test/java/academy/devdojo/utils/UserUtils.java
+++ user-service/src/test/java/academy/devdojo/utils/UserUtils.java
@@ -9,9 +9,9 @@ import java.util.List;
 @Component
 public class UserUtils {
     public List<User> newUserList() {
-        User marcelo = User.builder().id(1L).firstName("Marcelo").lastName("Viana").email("marcelo@gmai.com").build();
-        User vinicius = User.builder().id(2L).firstName("Vinicius").lastName("Lima").email("vinicius@gmai.com").build();
-        User mario = User.builder().id(3L).firstName("Mario").lastName("Lobado").email("mario@gmai.com").build();
+        User marcelo = User.builder().id(1L).firstName("Marcelo").lastName("Viana").email("marcelo@gmail.com").build();
+        User vinicius = User.builder().id(2L).firstName("Vinicius").lastName("Lima").email("vinicius@gmail.com").build();
+        User mario = User.builder().id(3L).firstName("Mario").lastName("Lobado").email("mario@gmail.com").build();
 
         return new ArrayList<>(List.of(marcelo, vinicius, mario));
     }
