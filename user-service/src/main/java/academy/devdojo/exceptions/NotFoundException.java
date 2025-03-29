2ac539fff1e32ceaf2c2ff54c6c90b5d66f2f8e6
mbranches <marcusvbranches@gmail.com>
2025-03-29 10:04:48 -0300

    feat: video66 - exceções customizadas adicionadas

---
+++ user-service/src/main/java/academy/devdojo/exceptions/NotFoundException.java
@@ -0,0 +1,11 @@
+package academy.devdojo.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.server.ResponseStatusException;
+
+public class NotFoundException extends ResponseStatusException {
+    public NotFoundException(String message) {
+        super(HttpStatus.NOT_FOUND, message);
+    }
+}
