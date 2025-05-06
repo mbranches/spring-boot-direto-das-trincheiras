c57086169334601bf120886a243447128dc121fe
mbranches <marcusvbranches@gmail.com>
2025-05-06 20:29:05 -0300

    feat: video93 - adiciona aula de relacionamento ManyToMany

---
+++ user-service/src/main/java/academy/devdojo/model/UserProfile.java
@@ -0,0 +1,21 @@
+package academy.devdojo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProfile {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(optional = false)
+    private User user;
+    @ManyToOne(optional = false)
+    private Profile profile;
+}
