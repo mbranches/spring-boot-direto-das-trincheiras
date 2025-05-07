2f6e73a193a70618ec3a3e26434be1b50511c74f
mbranches <marcusvbranches@gmail.com>
2025-05-06 22:08:56 -0300

    feat: video97 - adiciona query jpql

--- user-service/src/main/java/academy/devdojo/repository/UserProfileRepository.java
+++ user-service/src/main/java/academy/devdojo/repository/UserProfileRepository.java
@@ -2,7 +2,11 @@ package academy.devdojo.repository;
 
 import academy.devdojo.model.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
-
+    @Query(value = "SELECT up FROM UserProfile up JOIN FETCH up.user u JOIN FETCH up.profile p")
+    List<UserProfile> retrieveAll();
 }
