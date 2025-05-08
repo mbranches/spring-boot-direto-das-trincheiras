1aefc40d1182942fc31282907db7060230d38fbf
mbranches <marcusvbranches@gmail.com>
2025-05-08 12:51:38 -0300

    feat: video97 - otimiza query com EntityGraph

--- user-service/src/main/java/academy/devdojo/model/UserProfile.java
+++ user-service/src/main/java/academy/devdojo/model/UserProfile.java
@@ -9,6 +9,10 @@ import lombok.*;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@NamedEntityGraph(
+        name = "UserProfile.fullDetails",
+        attributeNodes = {@NamedAttributeNode("user"), @NamedAttributeNode("profile")}
+)
 public class UserProfile {
     @EqualsAndHashCode.Include
     @Id
--- user-service/src/main/java/academy/devdojo/repository/UserProfileRepository.java
+++ user-service/src/main/java/academy/devdojo/repository/UserProfileRepository.java
@@ -1,6 +1,7 @@
 package academy.devdojo.repository;
 
 import academy.devdojo.model.UserProfile;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,4 +10,8 @@ import java.util.List;
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
     @Query(value = "SELECT up FROM UserProfile up JOIN FETCH up.user u JOIN FETCH up.profile p")
     List<UserProfile> retrieveAll();
+
+//    @EntityGraph(attributePaths = {"user", "profile"})
+    @EntityGraph(value = "UserProfile.fullDetails")
+    List<UserProfile> findAll();
 }
