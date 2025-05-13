e4486ed72140539a660dc709880f9f7bf474cc6c
mbranches <marcusvbranches@gmail.com>
2025-05-13 16:59:55 -0300

    feat: video102 - adiciona testes para userProfileRepository

---
+++ user-service/src/test/java/academy/devdojo/repository/UserProfileRepositoryTest.java
@@ -0,0 +1,50 @@
+package academy.devdojo.repository;
+
+import academy.devdojo.model.User;
+import academy.devdojo.model.UserProfile;
+import academy.devdojo.utils.UserUtils;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class UserProfileRepositoryTest {
+    @Autowired
+    private UserProfileRepository repository;
+
+    @Test
+    @DisplayName("findAll returns all userProfiles when successful")
+    @Order(1)
+    @Sql("/sql/init_user_profile_2_users_1_profile.sql")
+    void findAll_ReturnsAllUserProfiles_WhenSuccessful() {
+        List<UserProfile> response = repository.findAll();
+
+        Assertions.assertThat(response)
+                .isNotNull()
+                .isNotEmpty();
+    }
+
+    @Test
+    @DisplayName("findAll returns all users by profile id when successful")
+    @Order(2)
+    @Sql("/sql/init_user_profile_2_users_1_profile.sql")
+    void findAll_ReturnsAllUsersByProfileId_WhenSuccessful() {
+        Long profileId = 1L;
+        List<User> response = repository.findAllUsersByProfileId(profileId);
+
+        Assertions.assertThat(response)
+                .isNotNull()
+                .hasSize(2)
+                .doesNotContainNull()
+                .isNotEmpty();
+    }
+
+}
