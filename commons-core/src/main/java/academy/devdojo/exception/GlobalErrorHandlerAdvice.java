9bdec924019339746e7c14bda089bc152f40a2e3
mbranches <marcusvbranches@gmail.com>
2025-05-29 19:16:00 -0300

    feat: video122 - adiciona tratamento de exceção para validação de campos

--- commons-core/src/main/java/academy/devdojo/exception/GlobalErrorHandlerAdvice.java
+++ commons-core/src/main/java/academy/devdojo/exception/GlobalErrorHandlerAdvice.java
@@ -1,16 +1,22 @@
 package academy.devdojo.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class    GlobalErrorHandlerAdvice {
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<DefaultErrorMessage> handlerNotFoundException(NotFoundException e) {
+    public ResponseEntity<DefaultErrorMessage> handlerNotFoundException( NotFoundException e) {
         DefaultErrorMessage error = new DefaultErrorMessage(HttpStatus.NOT_FOUND.value(), e.getReason());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
@@ -19,5 +25,19 @@ public class    GlobalErrorHandlerAdvice {
     public ResponseEntity<DefaultErrorMessage> handlerSQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException e) {
         DefaultErrorMessage error = new DefaultErrorMessage(HttpStatus.BAD_REQUEST.value(), "Duplicated entry for one of the unique fields");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handlerMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        String defaultMessage = e.getBindingResult().getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.joining(","));
+
+        ApiError response = ApiError.builder()
+                .timestamp(OffsetDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME))
+                .status(HttpStatus.BAD_REQUEST.value())
+                .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .path(request.getRequestURI())
+                .message(defaultMessage)
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }
