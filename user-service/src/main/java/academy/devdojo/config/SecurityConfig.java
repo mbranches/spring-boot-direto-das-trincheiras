1552b6ef5142e4def809a8fa5f55e60c25f63ecb
mbranches <marcusvbranches@gmail.com>
2025-05-31 19:02:48 -0300

    chore: remove necessidade de qualquer autorização da página do swagger

--- user-service/src/main/java/academy/devdojo/config/SecurityConfig.java
+++ user-service/src/main/java/academy/devdojo/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package academy.devdojo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,10 +11,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private static final String[] WHITE_LIST = {"/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**"};
+
     @Bean
     public UserDetailsService userDetailsService( PasswordEncoder passwordEncoder ) {
         UserDetails user = User.withUsername("Joel")
@@ -26,6 +31,15 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(auth -> {
+            auth.requestMatchers(WHITE_LIST).permitAll()
+                    .anyRequest()
+                    .authenticated();
+        }).httpBasic(Customizer.withDefaults()).build();
     }
 
     @Bean
